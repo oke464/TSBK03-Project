1acb588bce88b2da7432bcbb714566d5c4989678
Oskar Andersson <oskan803@student.liu.se>
2020-11-19 19:53:22 +0100

    Separated initial function ad display function, added input processing

--- main.cpp
+++ main.cpp
@@ -1,46 +1,88 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <stdio.h>  
+#include <iostream>
+
+// Headers to functions below. Want to keep main at top. 
+GLFWwindow* initGLFW_GLAD();
+void display(GLFWwindow* window);
 
 int main(void)
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = nullptr;
+    
+    window = initGLFW_GLAD();
+    
     if (!window)
     {
-        glfwTerminate();
+        // Some error happened in init function
         return -1;
+    }
+
+    display(window);
+
+    glfwTerminate();
+    return 0;
+}
+
+GLFWwindow* initGLFW_GLAD()
+{
+    GLFWwindow* window;
+    /* Initialize the library */
+    if (!glfwInit())
+        return nullptr;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1080, 800, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        
+        glfwTerminate();
+        return nullptr;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    // Think you have to init glad after OpenGL context which is closest above function.
+    // Think you have to init glad after OpenGL context which is the two closest above function.
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        //std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return nullptr;
     } 
+    
+    return window;
+}
+
+// Simple input handling function
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void display(GLFWwindow* window)
+{
+    // Set backgroundcolor
+    glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        // Handle inputs
+        processInput(window);
+
+        // Render here 
+
+
+
+        // Clear buffer 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        // Swap front and back buffers 
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // Poll for and process events 
         glfwPollEvents();
-    }
 
-    glfwTerminate();
-    return 0;
+    }
 }
